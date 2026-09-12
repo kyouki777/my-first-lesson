00d963c205b56eb872deb0f6fed26ee87b38865f
Kyouki <emilyjo333@gmail.com>
2026-09-12 23:24:39 +0800

    removed ending

---
+++ GameManager.gd
@@ -0,0 +1,5 @@
+extends Node
+
+func go_to_chapter2():
+	get_tree().change_scene_to_file("res://Scenes/Chapter2_Scenes/main2.tscn")
+	print("chapter 2 func called")
--- Scripts/floor.gd
+++ Scripts/floor.gd
@@ -6,6 +6,7 @@ func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
 		return true
 	return false
 	
+	
 func _tile_data_runtime_update(coords: Vector2i, tile_data: TileData) -> void:
 	if coords in obstacle_objects.get_used_cells_by_id(0):
 		tile_data.set_navigation_polygon(0, null)
