002d07f5ebffc3121450a4c7543caf82afa4a174
Kyouki <emilyjo333@gmail.com>
2026-09-14 03:15:58 +0800

    sound system updated

---
+++ Objects/player2.gd
@@ -0,0 +1,80 @@
+extends CharacterBody2D
+
+@export var walk_speed: float = 100.0
+@onready var floor: TileMapLayer = $"../Floor"
+
+@onready var anim_player: AnimationPlayer = $sprite2/AnimationPlayer
+
+var last_anim_direction: String = "Down"
+var last_dir: Vector2
+
+var is_walking : bool = false
+var current_surface: String = "wood"
+
+func _ready() -> void:
+	pass
+func _physics_process(delta):
+	
+	
+	var direction = Input.get_vector(
+		"ui_left",
+		"ui_right",
+		"ui_up",
+		"ui_down"
+	)
+
+	last_dir = direction
+
+	# Movement
+	velocity = direction.normalized() * walk_speed
+	move_and_slide()
+
+	# Animation
+	update_animation(direction)
+	
+	is_walking = direction != Vector2.ZERO
+	
+	if is_walking:
+		if not AudioManager.footsteps_audio.playing:
+			AudioManager.play_footsteps(current_surface)
+	else:
+		AudioManager.stop_footsteps()
+
+func update_animation(direction: Vector2):
+	var anim_to_play: String = ""
+	var new_anim_direction: String = last_anim_direction
+
+	if direction != Vector2.ZERO:
+		update_surface()
+		# Determine whether the player is moving
+		# horizontally or vertically
+		if abs(direction.x) > abs(direction.y):
+			new_anim_direction = "Right" if direction.x > 0 else "Left"
+		else:
+			new_anim_direction = "Down" if direction.y > 0 else "Up"
+
+		# Walking animation
+		anim_to_play = new_anim_direction
+
+		# Remember the direction
+		last_anim_direction = new_anim_direction
+
+	else:
+		# Idle animation
+		anim_to_play = "Idle_" + last_anim_direction
+
+	# Only change animation if necessary
+	if anim_player.current_animation != anim_to_play:
+		anim_player.play(anim_to_play)
+		
+#detects which surface the player is on
+func update_surface():
+	var tile_position = floor.local_to_map(floor.to_local(global_position))
+	
+	var tile_data = floor.get_cell_tile_data(tile_position)
+	
+	if tile_data:
+		current_surface = tile_data.get_custom_data("footstep_type")
+		#print("current surface: " + current_surface)
+	
+	
--- Scripts/audio_manager.gd
+++ Scripts/audio_manager.gd
@@ -3,21 +3,32 @@ extends Node2D
 ## 🔊 This is your library of all sound effects.
 ## Drag your sound files from the FileSystem dock into the "Value" part.
 
+@onready var footsteps_audio: AudioStreamPlayer = $FootstepsAudio
+
 # AudioManager.play_sfx("button_click")
 @export var sfx_library: Dictionary[String, AudioStream] = {
-	"clicksfx": preload("res://Assets/sounds/sounds/Click.mp3"),
+	"clicksfx": preload("res://Assets/sounds/sounds/Click_SFX.mp3"),
 	"lol":preload("uid://qb65ufe4oef4"),
-	"power":preload("res://Assets/output.mp3")
+	"power":preload("res://Assets/output.mp3"),
+	"footsteps":preload("uid://cf6aqrhkypacx"),
+	"heartbeat":preload("uid://348ylsy7towo"),
+	"jumpscare":preload("uid://cebakduu3fou7"),
+	"woodmash":preload("uid://bv57adc0yejs4")
 }
 
 # This will hold all our AudioStreamPlayer nodes
 var sfx_players: Array[AudioStreamPlayer2D] = []
 
 
+
 func _ready():
 	for child in get_children():
 		if child is AudioStreamPlayer2D:
 			sfx_players.append(child)
+
+
+func _process(delta):
+	pass
 
 
 ## 🎵 This is the only function you'll ever need to call from other scripts.
@@ -39,3 +50,36 @@ func play_sfx(sound_name: String):
 			
 	# 5. (Optional) If we get here, all players are busy.
 	print("Warning: All SFX players are busy. Sound not played: ", sound_name)
+	
+func play_footsteps(surface: String):
+	var current_surface_footsteps = str("Footsteps"+surface)
+	footsteps_audio["parameters/switch_to_clip"] = current_surface_footsteps
+	if not footsteps_audio.playing:
+		footsteps_audio.play()
+	print("audiomanager:" + current_surface_footsteps)
+	
+func stop_footsteps():
+	if footsteps_audio.playing:
+		footsteps_audio.stop()
+
+#func play_footsteps():
+	#var sound_name = current_footstep_surface + "_footsteps"
+#
+	#if not footsteps_audio.playing:
+		#footsteps_audio.stream = sfx_library[sound_name]
+		#footsteps_audio.play()
+#
+#
+#func stop_footsteps():
+	#if footsteps_audio.playing:
+		#footsteps_audio.stop()
+#
+#
+#func set_footstep_surface(surface: String):
+	#if current_footstep_surface != surface:
+		#current_footstep_surface = surface
+#
+		## Change sound immediately if currently walking
+		#if footsteps_audio.playing:
+			#footsteps_audio.stream = sfx_library[surface + "_footsteps"]
+			#footsteps_audio.play()
